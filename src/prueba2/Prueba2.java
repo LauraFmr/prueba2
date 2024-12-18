0080b14db99520ceec975cf4474844c24ac246a7
Laura Fernandez <laurafmr90@gmail.com>
2024-12-18 10:50:17 +0100

    Primer commit en tutorial

---
+++ src/prueba2/Prueba2.java
@@ -0,0 +1,14 @@
+package prueba2;
+
+public class Prueba2 {
+
+	public Prueba2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
